3bdf4b1e1facdc894135bd8d408628f4d852ebbf
bbhhffdesign <bbhhff.design@gmail.com>
2024-08-22 18:44:07 -0300

    aaaaa

---
+++ js/app.js
@@ -0,0 +1,13 @@
+const options = {
+    particles:{
+        number:{
+            value:100
+        },
+        color:"#333",
+        zIndex:{
+            value: -1
+        }
+    }
+}
+
+tsParticles.load("bgHero", options)
