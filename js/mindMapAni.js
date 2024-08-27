eeb0a6d425a4c498ebb81b28ffb9906d90ab0609
bauhoffer mariano <bauhoffermariano@gmail.com>
2024-08-27 01:10:20 -0300

    a

--- js/mindMapAni.js
+++ js/mindMapAni.js
@@ -1,31 +1,30 @@
-const heroContainer = document.getElementById("heroContainer")
+const heroContainer = document.getElementById("heroContainer");
 
 //texto
 const dueño = document.getElementById("owner");
-const agenda= document.getElementById("agenda");
-const posicionarMarca= document.getElementById("posicionarMarca");
-const branding= document.getElementById("branding");
-const excelente= document.getElementById("excelente");
-const aumentarVentas= document.getElementById("aumentarVentas");
-const agendaBig= document.getElementById("agendaBig");
-const agendaBigBg= document.getElementById("agendaBigBg");
-const estrategia= document.getElementById("estrategia");
-const camino= document.getElementById("camino");
+const agenda = document.getElementById("agenda");
+const posicionarMarca = document.getElementById("posicionarMarca");
+const branding = document.getElementById("branding");
+const excelente = document.getElementById("excelente");
+const aumentarVentas = document.getElementById("aumentarVentas");
+const agendaBig = document.getElementById("agendaBig");
+const agendaBigBg = document.getElementById("agendaBigBg");
+const estrategia = document.getElementById("estrategia");
+const camino = document.getElementById("camino");
 
 //si/no
-const si1= document.getElementById("si1");
-const si2= document.getElementById("si2");
-const si3= document.getElementById("si3");
-const no1= document.getElementById("no1");
-const no2= document.getElementById("no2");
-const no3= document.getElementById("no3");
+const si1 = document.getElementById("si1");
+const si2 = document.getElementById("si2");
+const si3 = document.getElementById("si3");
+const no1 = document.getElementById("no1");
+const no2 = document.getElementById("no2");
+const no3 = document.getElementById("no3");
 
-// flechas recorrido principal 
+// flechas recorrido principal
 //loopeo entre los elementos con id flechaX y les cambio la propiedad del trazo
 for (let i = 1; i <= 8; i++) {
   const flecha = document.getElementById(`flecha${i}`);
   const flechaLength = flecha.getTotalLength();
-
 
   flecha.style.strokeDasharray = flechaLength;
   flecha.style.strokeDashoffset = flechaLength;
@@ -33,7 +32,7 @@ for (let i = 1; i <= 8; i++) {
   window[`flecha${i}`] = flecha;
 }
 
-// //tween de flecha que me quedo al reves 
+// //tween de flecha que me quedo al reves
 
 // flechaFinal
 for (let i = 1; i <= 6; i++) {
@@ -48,131 +47,102 @@ for (let i = 1; i <= 6; i++) {
 const iconoLike = document.getElementById("iconoLike");
 const iconoTick = document.getElementById("iconoTick");
 
-
-
 //logos+array
-const logoYamatec= document.getElementById("logoYamatec");
-const logoUdana= document.getElementById("logoUdana");
-const logoSound= document.getElementById("logoSound");
-const logoX= document.getElementById("logoX");
-const logoBelen= document.getElementById("logoBelen");
-const logoGallmarquez= document.getElementById("logoGallmarquez");
-const arrayLogo = [logoYamatec, logoUdana, logoSound, logoX, logoBelen, logoGallmarquez];
+const logoYamatec = document.getElementById("logoYamatec");
+const logoUdana = document.getElementById("logoUdana");
+const logoSound = document.getElementById("logoSound");
+const logoX = document.getElementById("logoX");
+const logoBelen = document.getElementById("logoBelen");
+const logoGallmarquez = document.getElementById("logoGallmarquez");
+const arrayLogo = [
+  logoYamatec,
+  logoUdana,
+  logoSound,
+  logoX,
+  logoBelen,
+  logoGallmarquez,
+];
 
 let mmMobile = gsap.matchMedia();
 let mmDesktop = gsap.matchMedia();
 
+let funcTimelines = function (timelines) {
+  timelines
+    .from(dueño, { opacity: 0 })
+    .to(flecha1, { strokeDashoffset: 0 })
+    .from(no1, { opacity: 0 }, "<50%")
+    .from(agenda, { opacity: 0 })
+    .to(flecha2, { strokeDashoffset: 0 })
+    .from(si1, { opacity: 0 }, "<50%")
+    .from(posicionarMarca, { opacity: 0 })
+    .to(flecha3, { strokeDashoffset: 0 })
+    .from(si2, { opacity: 0 }, "<50%")
+    .from(branding, { opacity: 0 })
+    .to(flecha4, { strokeDashoffset: 0 })
+    .from(no2, { opacity: 0 }, "<50%")
+    .from(excelente, { opacity: 0 })
+    .from(
+      iconoLike,
+      {
+        opacity: 0,
+        scale: 0.5,
+        transformOrigin: "50% 50%",
+        ease: "bounce.out",
+        duration: 0.8,
+      },
+      "<"
+    )
+    .from(aumentarVentas, { opacity: 0 })
+    .to(flecha5, { strokeDashoffset: 0 })
+    .from(si3, { opacity: 0 }, "<50%")
+    .from(estrategia, { opacity: 0 })
+    .to(flecha6, { strokeDashoffset: 0 })
+    .to(flecha7, { strokeDashoffset: 0 }, "<")
+    .from(agendaBig, { opacity: 0 })
+    .from(agendaBigBg, { opacity: 0 }, "<")
+    .to(flecha8, { strokeDashoffset: 0 })
+    .from(no3, { opacity: 0 }, "<10%")
+    .from(camino, { opacity: 0 })
+    .from(
+      iconoTick,
+      {
+        opacity: 0,
+        scale: 0.5,
+        transformOrigin: "50% 50%",
+        ease: "bounce.out",
+        duration: 0.8,
+      },
+      "<"
+    )
+
+    .to(flechaF1, { strokeDashoffset: 0 }, "<")
+    .from(logoYamatec, { opacity: 0 })
+    .to(flechaF2, { strokeDashoffset: 0 }, "<")
+    .from(logoUdana, { opacity: 0 })
+    .to(flechaF3, { strokeDashoffset: 0 }, "<")
+    .from(logoSound, { opacity: 0 }, "<+80%")
+    .to(flechaF4, { strokeDashoffset: 0 }, "<")
+    .from(logoX, { opacity: 0 }, "<+80%")
+    .to(flechaF5, { strokeDashoffset: 0 }, "<")
+    .from(logoBelen, { opacity: 0 }, "<+80%")
+    .to(flechaF6, { strokeDashoffset: 0 }, "<")
+    .from(logoGallmarquez, { opacity: 0 }, "<+80%");
+};
+
 mmMobile.add("(max-width: 768px)", () => {
-    
-
-    let tlMindmap = gsap.timeline({
-        scrollTrigger:{
-          trigger: heroContainer,
-          scrub: true,
-          markers:true,
-          pin:true,
-        //   pinSpacing:true,
-          start: "25% top",
-          end: "500% 20%"
-        }
-      });
-
-      tlMindmap.from(dueño,{opacity:0})
-      .to(flecha1,{strokeDashoffset:0})
-      .from(no1, {opacity:0},"<50%")
-      .from(agenda,{opacity:0})
-      .to(flecha2,{strokeDashoffset:0})
-      .from(si1, {opacity:0},"<50%")
-      .from(posicionarMarca,{opacity:0})
-      .to(flecha3,{strokeDashoffset:0})
-      .from(si2, {opacity:0},"<50%")
-      .from(branding,{opacity:0})
-      .to(flecha4,{strokeDashoffset:0})
-      .from(no2, {opacity:0},"<50%")
-      .from(excelente,{opacity:0})
-      .from(iconoLike,{opacity:0,scale:.5,transformOrigin:"50% 50%", ease:"bounce.out",duration:.8},"<")
-      .from(aumentarVentas,{opacity:0})
-      .to(flecha5,{strokeDashoffset:0})
-      .from(si3, {opacity:0},"<50%")
-      .from(estrategia,{opacity:0})
-      .to(flecha6,{strokeDashoffset:0})
-      .to(flecha7,{strokeDashoffset:0},"<")
-      .from(agendaBig,{opacity:0})
-      .from(agendaBigBg,{opacity:0},"<")
-      .to(flecha8,{strokeDashoffset:0})
-      .from(no3, {opacity:0},"<10%")
-      .from(camino,{opacity:0})
-      .from(iconoTick,{opacity:0,scale:.5,transformOrigin:"50% 50%", ease:"bounce.out",duration:.8},"<")
- 
-      .to(flechaF1,{strokeDashoffset:0},"<")
-      .from(logoYamatec,{opacity:0},)
-      .to(flechaF2,{strokeDashoffset:0},"<")
-      .from(logoUdana,{opacity:0},)
-      .to(flechaF3,{strokeDashoffset:0},"<")
-      .from(logoSound,{opacity:0},"<+80%")
-      .to(flechaF4,{strokeDashoffset:0},"<")
-      .from(logoX,{opacity:0},"<+80%")
-      .to(flechaF5,{strokeDashoffset:0},"<")
-      .from(logoBelen,{opacity:0},"<+80%")
-      .to(flechaF6,{strokeDashoffset:0},"<")
-      .from(logoGallmarquez,{opacity:0},"<+80%")  
+  let tlMindmap = gsap.timeline({
+    scrollTrigger: {
+      trigger: heroContainer,
+      scrub: true,
+      markers: true,
+      pin: true,
+      //   pinSpacing:true,
+      start: "25% top",
+      end: "500% bottom",
+    },
   });
 
-  mmDesktop.add("(min-width: 800px)", () => {
+  funcTimelines(tlMindmap)
 
-    let tlMindmap2 = gsap.timeline({
-        scrollTrigger:{
-          trigger: heroContainer,
-          scrub: true,
-          markers:true,
-          pin:true,
-          pinSpacing:true,
-          start: "top top",
-          end: "500% center"
-        }
-      });
 
-     tlMindmap2.from(dueño,{opacity:0})
-              .to(flecha1,{strokeDashoffset:0})
-              .from(no1, {opacity:0},"<50%")
-              .from(agenda,{opacity:0})
-              .to(flecha2,{strokeDashoffset:0})
-              .from(si1, {opacity:0},"<50%")
-              .from(posicionarMarca,{opacity:0})
-              .to(flecha3,{strokeDashoffset:0})
-              .from(si2, {opacity:0},"<50%")
-              .from(branding,{opacity:0})
-              .to(flecha4,{strokeDashoffset:0})
-              .from(no2, {opacity:0},"<50%")
-              .from(excelente,{opacity:0})
-              .from(iconoLike,{opacity:0,scale:.5,transformOrigin:"50% 50%", ease:"bounce.out",duration:.8},"<")
-              .from(aumentarVentas,{opacity:0})
-              .to(flecha5,{strokeDashoffset:0})
-              .from(si3, {opacity:0},"<50%")
-              .to("#mindMap",{y:-100},"<")
-              .from(estrategia,{opacity:0})
-              .to(flecha6,{strokeDashoffset:0})
-              .to(flecha7,{strokeDashoffset:0},"<")
-              .from(agendaBig,{opacity:0})
-              .from(agendaBigBg,{opacity:0},"<")
-              .to(flecha8,{strokeDashoffset:0})
-              .from(no3, {opacity:0},"<10%")
-              .to("#mindMap",{y:-200},"<")
-              .from(camino,{opacity:0})
-              .from(iconoTick,{opacity:0,scale:.5,transformOrigin:"50% 50%", ease:"bounce.out",duration:.8},"<")
-    
-
-               .to("#mindMap",{y:-350},"<")
-              .to(flechaF1,{strokeDashoffset:0},"<")
-              .from(logoYamatec,{opacity:0},"<")
-              .to(flechaF2,{strokeDashoffset:0},"<")
-              .from(logoUdana,{opacity:0},"<")
-              .to(flechaF3,{strokeDashoffset:0},"<")
-              .from(logoSound,{opacity:0},"<")
-              .to(flechaF4,{strokeDashoffset:0},"<")
-              .from(logoX,{opacity:0},"<")
-              .to(flechaF5,{strokeDashoffset:0},"<")
-              .from(logoBelen,{opacity:0},"<")
-              .to(flechaF6,{strokeDashoffset:0},"<")
-              .from(logoGallmarquez,{opacity:0},"<+80%")
-    });
+});
